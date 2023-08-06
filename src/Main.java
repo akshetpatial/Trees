7aaf8634c0d04d1b7f258fcc144f53680361feaa
akshetpatial <akshet.patial@gmail.com>
2023-08-06 14:40:25 +0530

    Created the BinaryTrees.java class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BinaryTrees binaryTrees = new BinaryTrees();
+
+        Scanner scan = new Scanner(System.in);
+//        System.out.println("Press yes to add in left");
+//        System.out.println("Press no to add in right");
+
+//        for( int i = 0 ; i < count; i++) {
+//           System.out.println("Enter your choice: ");
+//            String check = scan.next();
+//            if(check == "yes") {
+//                System.out.println("Enter the element: ");
+//               int element = scan.nextInt();
+////                binaryTrees.add(element,true, count);
+//            }
+//            else{
+//                System.out.println("Enter the element: ");
+//                int element = scan.nextInt();
+
+        System.out.println("Enter the Root Element: ");
+        int element = scan.nextInt();
+            binaryTrees.add(element);
+//            }
+//
+//        }
+
+        System.out.println("Display Tree: ");
+        binaryTrees.display();
     }
 }
